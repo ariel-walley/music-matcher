9f6363d9aba82b95dd5d9bdc1663f4742fa38678
Ariel Walley <arielwalley95@gmail.com>
2020-06-20 15:57:48 -0400

    Request access token

--- src/home.js
+++ src/home.js
@@ -1,23 +1,44 @@
 import React from 'react';
+import QueryString from 'querystring';
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {playlists: [] };
+        this.state = {
+          playlists: [],
+          accessToken: ''
+        };
         this.getUserMusic = this.getUserMusic.bind(this);
+        this.getAccessToken = this.getAccessToken.bind(this);
     }
 
-    componentDidMount() {
-      this.getUserMusic();
+    async getAccessToken() {
+      let body = {
+        'grant_type': 'client_credentials'
+      }
+
+      await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + window.btoa(process.env.REACT_APP_CLIENT_ID + ':' + process.env.REACT_APP_SECRET_KEY)
+        },
+        body: QueryString.stringify(body)
+      }).then((response) => {
+        return response.json();
+      }).then((data) => {
+        this.setState({accessToken: data.access_token});
+      }).catch((err) => {
+        console.log(err);
+      })
     }
 
     getUserMusic() {
-      let accessToken = 'BQCwHJ2mraqflmKkbxBq6ZUO3A7CR79odHhj5PwO4oby6LBjBiI3nUxcbV99eAxEaOlOKtZv2h7eXECagOs';
       let userName = 'emilytcarlsen';
       
       fetch('https://api.spotify.com/v1/users/' + userName + '/playlists?limit = 50', {
         headers: {
-          'Authorization': 'Bearer ' + accessToken
+          'Authorization': 'Bearer ' + this.state.accessToken
           }
         }).then((response) => {
           return response.json();
@@ -26,6 +47,11 @@ class Home extends React.Component {
         }).catch((err) => {
           console.log(err);
         });
+    }
+
+    async componentDidMount() {
+      await this.getAccessToken();
+      this.getUserMusic();
     }
 
     render () {
