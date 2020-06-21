24b2dca3ee7bb0da4368d04d6eeeb593584d841a
Ariel Walley <arielwalley95@gmail.com>
2020-06-21 17:54:32 -0400

    Update state with JSON data
    Update state with user info and user playlist info from API call in JSON format

--- src/home.js
+++ src/home.js
@@ -5,11 +5,15 @@ class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          playlists: [],
-          accessToken: ''
+          accessToken: '',
+          fieldInput: '',
+          userPlaylists: {}
         };
-        this.getUserMusic = this.getUserMusic.bind(this);
         this.getAccessToken = this.getAccessToken.bind(this);
+        this.getUserMusic = this.getUserMusic.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.prepareUsers = this.prepareUsers.bind(this);
+        this.preparePlaylists = this.preparePlaylists.bind(this);
     }
 
     async getAccessToken() {
@@ -33,48 +37,80 @@ class Home extends React.Component {
       })
     }
 
-    getUserMusic() {
-      let userName = 'emilytcarlsen';
-      
+    usernames = ['ariel.walley', 'emilytcarlsen'];
+
+    prepareUsers(usernames) {
+      let users = {}
+      for (let i = 0; i < usernames.length; i++) {
+        users[usernames[i]] = {}
+      }
+      this.setState({
+        userPlaylists: users
+      });
+    }
+
+    preparePlaylists(data, userName) {
+     
+      let playlists = {};
+      for (let i = 0; i < data.length; i++) {
+        playlists[data[i].name] = [];
+      }
+      let userPlaylistsLocal = this.state.userPlaylists
+      userPlaylistsLocal[userName] = playlists
+      this.setState({ 
+        userPlaylists: userPlaylistsLocal
+      });
+    }
+         
+    getUserMusic(userName) {    
       fetch('https://api.spotify.com/v1/users/' + userName + '/playlists?limit = 50', {
         headers: {
           'Authorization': 'Bearer ' + this.state.accessToken
           }
         }).then((response) => {
           return response.json();
-        }).then((data) => {
-          this.setState({playlists: data.items});
+        }).then((response) => {
+          this.preparePlaylists(response.items, userName);
         }).catch((err) => {
           console.log(err);
         });
     }
-
+    
     async componentDidMount() {
+      this.prepareUsers(this.usernames);
       await this.getAccessToken();
-      this.getUserMusic();
+      this.getUserMusic('emilytcarlsen');
+      this.getUserMusic('ariel.walley');
+
     }
 
+    handleChange(event) {
+      this.setState({fieldInput: event.target.value});
+      console.log(this.state.fieldInput);
+
+    }
+   
     render () {
       return (
         <div>
           <p>This is the home page!</p>
-          <p>List of Emily's Playlists</p>
+          <label>What user do you want to search for?</label>
+          <input type='text' value={this.state.title} onChange={this.handleChange}/>
+          <p>List of Ariel's Playlists</p>
+          {/* {this.state.userPlaylists.emilytcarlsen.map(playlist => (
+            <li key={playlist.id}>{playlist}</li>
+          ))}
           <ul>
-            {this.state.playlists.map(playlist => (
-            <li key={playlist.id}>{playlist.name}</li>
+            {this.state.userPlaylists.emilytcarlsen.map(playlist => (
+            <li key={playlist.id}>{playlist}</li>
             ))}
           </ul>
+           <p>List of Emily's Playlists</p>
+          <ul>
+            </ul> */}
         </div>
       );
     }
 };
-
-
-
-
-
-
-
-
 
 export default Home;
