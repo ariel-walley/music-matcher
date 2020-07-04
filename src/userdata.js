e7c146c93e0747d6b67a5dd51398972f89b3a2bd
Ariel Walley <arielwalley95@gmail.com>
2020-07-04 00:11:31 -0400

    Stylized duplicate songs cards
    Installed styled-component module and wrote CSS code for duplicate cards

--- src/userdata.js
+++ src/userdata.js
@@ -1,4 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  width: 50%;
+  margin: 0 auto 10px auto;
+  align-items: center;
+  align-content: center;
+  justify-content: flex-start;
+  display: flex;
+  background-image: radial-gradient( circle farthest-corner at 10% 20%,  rgba(243,94,131,1) 17.1%, rgba(236,212,80,1) 89.7% );
+  border-radius: 25px;
+  padding: 18px;
+`;
+
+const SongTitle = styled.h1`
+  font-size: 24px;
+  font-weight: 700px;
+  margin: 0 0 10px 25px;
+  padding: 0;
+`;
+
+const Artist = styled.h2`
+  font-size: 20px;
+  font-weight: 550px;
+  margin: 0 0 0 25px;
+  padding: 0;
+`;
+
+const Img = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 10px;
+  margin: 10 18px 10 10;
+
+`;
 
 class DisplayData extends React.Component {
   constructor(props) {
@@ -7,19 +42,18 @@ class DisplayData extends React.Component {
     this.renderData = this.renderData.bind(this);
     }
 
-
-    formatData(){
+    formatData() {
       let display = [];
       this.props.data.duplicateData.map((duplicate) => {
-        console.log(duplicate);
-        console.log(duplicate.id, duplicate.name);
         display.push(
-        <div>
-          <li><h1 key={duplicate.id}>{duplicate.name}</h1></li><h2> by {duplicate.artist}</h2>
-          <img src={duplicate.image} alt="album image"/>
-        </div>); 
+        <Div>
+          <Img src={duplicate.image} alt="album image"/>
+          <div>
+            <SongTitle>{duplicate.name}</SongTitle>
+            <Artist> by {duplicate.artist}</Artist>
+          </div>
+        </Div>); 
       })
-      console.log(display);
       return display;
     }
 
