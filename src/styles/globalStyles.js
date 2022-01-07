06c49cae4e056a93449e54e176d3e37debee212e
Ariel Walley <arielwalley95@gmail.com>
2022-01-07 09:35:04 -0500

    Combined index.css and globalStyles.js

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,23 +1,27 @@
 import { createGlobalStyle } from 'styled-components';
  
 const GlobalStyle = createGlobalStyle`
-html {
-  height: 100%;
-  width: 100%;
-}
+  html {
+    height: 100%;
+    width: 100%;
+  }
 
-body {
-  height: 100%;
-  width: 100%;
-  margin: 0 auto;
-  padding: 0;
-  font-family: "Open Sans", Arial, sans-serif;
+  #root {
+    height: 100%;
+  }
 
-}
+  body {
+    height: 100%;
+    width: 100%;
+    margin: 0 auto;
+    padding: 0;
+    font-family: "Open Sans", Arial, sans-serif;
 
-span {
-  text-decoration: underline;
-}
+  }
+
+  span {
+    text-decoration: underline;
+  }
 `;
 
 export default GlobalStyle;
